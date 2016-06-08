78c156d40182ba0aa4ef5979f812ea939a9c1a9a
slavidichkov <dichkof@gmail.com>
2016-06-08 11:05:41 +0300

    deleted unnecessary files

--- frontend/src/app/app.spec.js
+++ frontend/src/app/app.spec.js
@@ -1,16 +1,16 @@
-// describe( 'AppCtrl', function() {
-//   describe( 'isCurrentUrl', function() {
-//     var AppCtrl, $scope;
-//
-//     beforeEach( module( 'bank' ) );
-//
-//     beforeEach( inject( function( $controller, $rootScope ) {
-//       $scope = $rootScope.$new();
-//       AppCtrl = $controller( 'AppCtrl', { $scope: $scope });
-//     }));
-//
-//     it( 'app test', inject( function() {
-//       expect( true ).toBeTruthy();
-//     }));
-//   });
-// });
+ describe( 'AppCtrl', function() {
+   describe( 'isCurrentUrl', function() {
+     var AppCtrl, $scope;
+
+     beforeEach( module( 'bank' ) );
+
+     beforeEach( inject( function( $controller, $rootScope ) {
+       $scope = $rootScope.$new();
+       AppCtrl = $controller( 'AppCtrl', { $scope: $scope });
+     }));
+
+     it( 'app test', inject( function() {
+       expect( true ).toBeTruthy();
+     }));
+   });
+ });
